dd30fce2c944e563e9d83efa5dc3c2b13cb7d42b
Tecla <teclahorece@gmail.com>
2026-04-02 05:04:27 +0300

    Temprarrily disable images to test

--- webhook.js
+++ webhook.js
@@ -380,12 +380,12 @@ const propertyMessage =
 
               console.log(`Sending property ${i + 1}: ${property.property_name}`);
 
-              await sendMessage(
-              tenantWhatsApp,
-              from,
-              propertyMessage,
-              property.photo_url || null
-            );
+             await sendMessage(
+  tenantWhatsApp,
+  from,
+  propertyMessage,
+  null  // temporarily disabled to test
+);
 
               console.log(`Property ${i + 1} sent successfully`);
 
